78004777c19807250648a4d0ee92a215224437b7
Abhay Dekate <abhaydekate@abhays-air.home>
2024-02-18 18:26:55 +0000

    Remove frontend1

--- frontend1/src/store/helpers.tsx
+++
@@ -1,8 +0,0 @@
-export const getFileExtension = (fileName: string) => {
-  return fileName.slice(((fileName.lastIndexOf(".") - 1) >>> 0) + 2);
-};
-
-export const getImageBucketPath = (signedUrl: string) => {
-  const match = signedUrl.match(/https?:\/\/[^\/]+(\/[^?]+)?/);
-  return match ? match[1] : "";
-};
